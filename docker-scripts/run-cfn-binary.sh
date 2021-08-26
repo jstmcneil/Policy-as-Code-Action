72c2788c502f073f14c928f7b647ab1e54e195c6
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-08-26 16:26:45 -0400

    Update run-cfn-binary.sh

--- docker-scripts/run-cfn-binary.sh
+++ docker-scripts/run-cfn-binary.sh
@@ -10,10 +10,9 @@ fi
 
 mkdir results
 echo "Files in order of scanned: "
-find . -type f \( -iname \*.yaml -o -iname \*.json -o -iname \*.yml \) -follow | while read -d $'\0' f
+find . -type f \( -iname \*.yaml -o -iname \*.json -o -iname \*.yml \) -follow -print0 | while read -d $'\0' f
 do
   base_name="$(basename -- $f)"
-  echo $base_name
   python $python_script $f
   ret=$?
   if [[ $ret -eq 0 ]]; then
@@ -21,6 +20,7 @@ do
   	/scripts/cfn-guard-data-wrangle.sh $outputted $f >> results/${base_name}.txt
     numb="$(cat results/${base_name}.txt | grep "Resource \[" | wc -l)"
     if [[ $numb -ne 0 ]]; then
+      echo $base_name
       echo -e "Total Failures: "  | sed -e '$s%$%'"$numb"'%' >> results/${base_name}.txt
     else
       rm results/${base_name}.txt
